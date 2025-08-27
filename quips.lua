e9c7765486785325e57f3296911ce8dc4664ed0e
V--R <vrart1@hotmail.com>
2025-08-27 19:06:23 -0400

    Add files via upload

---
+++ quips.lua
@@ -0,0 +1,53 @@
+-- Character = Key
+Fizzarolli = 'c_fool'
+Octavia = 'c_magician'
+SallieMay = 'c_high_priestess'
+Millie = 'c_empress'
+Moxxie = 'c_emperor'
+Andrealphus = 'c_hierophant'
+Verosika = 'c_lovers'
+Stella = 'c_chariot'
+Satan = 'c_justice'
+Mammon = 'c_hermit'
+Beelzebub = 'c_wheel_of_fortune'
+Loona = 'c_strength'
+Striker = 'c_hanged_man'
+GlitzGlam = 'c_death'
+Vortex = 'c_temperance'
+Lucifer = 'c_devil'
+Crimson = 'c_tower'
+Blitzo = 'c_star'
+Barbie = 'c_moon'
+Asmodeus = 'c_sun'
+HumanLoona = 'c_judgement'
+Stolas = 'c_world'
+
+-- Wins
+
+SMODS.JimboQuip{ key = 'fizzwin',
+    type = 'win',
+	extra = { center = Fizzarolli, particle_colours = { G.C.RED, G.C.RED, G.C.BLACK }, materialize_colours = { G.C.RED, G.C.RED, G.C.BLACK} }, sound = 'voice11'}
+SMODS.JimboQuip{ key = 'mammonwin',
+    type = 'win',
+	extra = { center = Mammon, particle_colours = { G.C.RED, G.C.RED, G.C.BLACK }, materialize_colours = { G.C.RED, G.C.RED, G.C.BLACK} }, sound = 'voice11'}
+
+-- Losses
+
+SMODS.JimboQuip{ key = 'blitzoloss1',
+    type = 'loss',
+	extra = { center = Blitzo, particle_colours = { G.C.RED, G.C.RED, G.C.BLACK }, materialize_colours = { G.C.RED, G.C.RED, G.C.BLACK} }, sound = 'voice11'}
+SMODS.JimboQuip{ key = 'blitzoloss2',
+    type = 'loss',
+	extra = { center = Blitzo, particle_colours = { G.C.RED, G.C.RED, G.C.BLACK }, materialize_colours = { G.C.RED, G.C.RED, G.C.BLACK} }, sound = 'voice11'}
+SMODS.JimboQuip{ key = 'fizzloss',
+    type = 'loss',
+	extra = { center = Fizzarolli, particle_colours = { G.C.RED, G.C.RED, G.C.BLACK }, materialize_colours = { G.C.RED, G.C.RED, G.C.BLACK} }, sound = 'voice11'}
+SMODS.JimboQuip{ key = 'glitzglamloss',
+    type = 'loss',
+	extra = { center = GlitzGlam, particle_colours = { G.C.RED, G.C.RED, G.C.BLACK }, materialize_colours = { G.C.RED, G.C.RED, G.C.BLACK} }, sound = 'voice11'}
+SMODS.JimboQuip{ key = 'octavialoss',
+    type = 'loss',
+	extra = { center = Octavia, particle_colours = { G.C.RED, G.C.RED, G.C.BLACK }, materialize_colours = { G.C.RED, G.C.RED, G.C.BLACK} }, sound = 'voice11'}
+SMODS.JimboQuip{ key = 'stolasloss',
+    type = 'loss',
+	extra = { center = Stolas, particle_colours = { G.C.RED, G.C.RED, G.C.BLACK }, materialize_colours = { G.C.RED, G.C.RED, G.C.BLACK} }, sound = 'voice11'}
